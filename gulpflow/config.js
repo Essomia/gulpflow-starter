1ae4cbf1d7ddc5078234e17d1923bd574395692c
Melissa Boutry <github@melissaboutry.com>
2018-09-02 10:17:54 -0400

    Create basic config.js for tasks

---
+++ gulpflow/config.js
@@ -0,0 +1,27 @@
+// ------------------------------
+// -- ./config.js
+// ------------------------------
+
+'use strict';
+
+//
+// Configurations
+//
+
+const pathRoot = {
+    src: './example/src',
+    dest: './example/dest'
+};
+
+const config = {
+
+    root: pathRoot
+
+};
+
+
+//
+// Exports
+//
+
+module.exports = config;
