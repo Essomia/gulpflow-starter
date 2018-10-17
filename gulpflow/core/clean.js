02a9ea7a58788b18cc941c7fd7e2cdcdd1807ed9
Melissa Boutry <essomia@users.noreply.github.com>
2018-10-16 22:49:15 -0400

    Remove log before /core/ runing tasks

--- gulpflow/core/clean.js
+++ gulpflow/core/clean.js
@@ -4,28 +4,18 @@
 
 'use strict';
 
-const config = require('../config');
-const gulp   = require('gulp');
-const colors = require('colors/safe');
-const del    = require('del');
-const log    = require('fancy-log');
+const config  = require('../config');
+const gulp    = require('gulp');
+const del     = require('del');
 
 
 //
 // Defined
 //
 
-function msgClean() {
-    log(colors.bgYellow(`##############################`));
-    log(colors.bgYellow(`#### CLEAN PROJECT ###########`));
-    log(colors.bgYellow(`##############################`));
-}
-
 function clean() {
 
     gulp.task('clean', () => {
-        msgClean();
-
         return del([ config.root.dest ]);
     });
 
