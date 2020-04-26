46db59bfdb5ead8087279965f1811c7473d0a6a3
Melissa Boutry <essomia@users.noreply.github.com>
2020-04-26 16:50:39 -0400

    Remove config file and move config object to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,45 @@
 // ------------------------------
 
 const plugins = require('gulp-load-plugins')();
-const config = require('gulpflow/config');
+
+//
+// Set default config
+//
+
+const config = {
+    root: {
+        yaml: './',
+        src: './example/src',
+        dest: './example/app'
+    },
+    ifs: {
+        doSourcemaps: false,
+        doMinify: true
+    },
+    yaml: {
+        eslint: '.eslintrc.yaml',
+        stylelint: '.stylelintrc.yaml'
+    },
+    // Object of {key}:{value}
+    // - {key} is the taskname
+    // - {value} is all path to watch for files
+    sources: {
+        js: '/components/**/js' + '/**/*.js',
+        scss: '/components/**/scss' + '/**/*.{sass,scss}',
+        images: '/components/**/assets/images' + '/**/*.{jpg,jpeg,png,gif,svg}',
+        fonts: '/components/**/assets/fonts' + '/**/*.{eot,svg,ttf,woff,woff2}',
+        html: '/components/**/assets/raw' + '/**/*.html',
+        css: '/components/**/assets/raw' + '/**/*.css',
+        json: '/components/**/assets/raw' + '/**/*.json'
+    },
+    // Object of {key}:{value}
+    // - {key} is the taskname
+    // - {value} is an array of tasks to run (see sources for tasks list)
+    tasks: {
+        watch: ['images', 'fonts', 'html', 'json', 'css', 'scss', 'js'],
+        build: ['images', 'fonts', 'html', 'json', 'css', 'scss', 'js']
+    }
+};
 
 
 //
