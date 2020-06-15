7626c154bb06416c553155e9600a836982a83c71
Melissa Boutry <essomia@users.noreply.github.com>
2020-06-15 18:47:39 -0400

    Adjust tasks to use util rename path

--- gulpflow/tasks/js.js
+++ gulpflow/tasks/js.js
@@ -1,4 +1,5 @@
 const filenameLog = require('../util/filenameLog');
+const renameDirPath = require('../util/renameDirPath');
 
 const js = (gulp, $, config) => {
     gulp.task('js', () => gulp
@@ -40,6 +41,16 @@ const js = (gulp, $, config) => {
         .pipe($.if(
             config.ifs.doSourcemaps,
             $.sourcemaps.write('.')
+        ))
+        .pipe($.if(
+            Boolean(config.destinations.js),
+            $.rename((path) => {
+                // eslint-disable-next-line no-param-reassign
+                path.dirname =
+                    config.destinations.js +
+                    renameDirPath(path.dirname, config.sources.js)
+                ;
+            })
         ))
         .pipe(gulp.dest(config.root.dest))
         .pipe(filenameLog()));
